c6ef3f7923cc29b4bf05fb09d3ba4fb325787a3a
kimtaeyoung <tkken12@naver.com>
2022-09-22 18:13:44 +0900

    테이블 바디 추가

--- src/component/sidebar/sidebarMenu.js
+++ src/component/sidebar/sidebarMenu.js
@@ -6,28 +6,37 @@ import { Link } from "react-router-dom"
 const SidebarMenu = () => { 
 
     const [ activeMenu, setActiveMenu ] = useState("")
+    const [ activeSubmenu, setActiveSubmenu ] = useState("")
     const activeHandler = e => { setActiveMenu(e.target.id) }
+    const activeSubmenuHandler = e => { setActiveSubmenu(e.target.id) }
 
     return (
         routePath.map( route => (
-            showMenus( route, activeMenu, activeHandler)
+            showMenus( route, activeMenu, activeSubmenu, activeHandler, activeSubmenuHandler )
         ))
     )
 }
 
-const showMenus = ( route, activeMenu, activeHandler ) => { 
+const showMenus = ( route, activeMenu, activeSubmenu, activeHandler, activeSubmenuHandler ) => { 
     if( route["type"] === "menu" ) {
         return (
             <div id={ route["key"] } className="sidebar" onClick={ activeHandler } >
-                {route["name"]}
+                { route["name"] }
                 <ul className="submenuFont">
                     { route["subMenus"].map( subMenu => ( 
-                        <Link to={  subMenu["path"]}>
-                            <li style={ activeMenu !== route["key"] ? { display: "none"} : {display: "flex"} } > { subMenu["name"] } </li>
+                        <Link to={ subMenu["path"] }>
+                            <li 
+                                style={ activeMenu !== route["key"] ? 
+                                        { display: "none" } 
+                                        :
+                                        { display: "flex" } 
+                                      } 
+                                onClick={ activeSubmenuHandler }
+                            > { subMenu["name"] } 
+                            </li>
                         </Link>
                     ))}
-                    </ul>
-
+                </ul>
             </div>
         )
     }
--- src/views/podInfo/podInfo.js
+++ src/views/podInfo/podInfo.js
@@ -1,11 +1,21 @@
-import React from "react";
+import TableBody from "component/table/tableBody";
+import { TableColumns } from "component/table/tableStyle";
+import React, { useState } from "react";
 import "views/podInfo/podInfoBody.css"
 
 const PodInfo = () => { 
 
+    const [ podInfo, setPodInfo ] = useState([])
+
+    console.log(TableColumns )
     return(
         <>
             <div className="podInfoBody">
+                <TableBody
+                    data = { podInfo }
+                    columns = { TableColumns() }
+                />
+
             </div>
             <div>
 
