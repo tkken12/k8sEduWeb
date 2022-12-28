d53341a4596c40564dcec76393d7a65ac044aa2b
kimtaeyoung <tkken12@naver.com>
2022-12-28 16:40:52 +0900

    노드 모니터링 코드 개선

--- src/component/table/dataGridBody.js
+++ src/component/table/dataGridBody.js
@@ -5,11 +5,13 @@ const MakeDataGrid = props => {
         <DataGrid 
             rows = { props.rows}
             columns ={ props.columns }
-            pageSize= { 5 }
+            pageSize= { 10 }
             rowsPerPageOptions={[5]}
             checkboxSelection = { props.checkboxSelection }
             rowHeight = { !!props.rowHeight !== false ? props.rowHeight : 52 }
             onRowClick = { !!props.onRowClick !== false ? props.onRowClick : null }
+            autoHeight ={ !!props.autoHeight !== false ? props.autoHeight : false }
+            // getRowHeight={() => 'auto'}
         />
     )
 } 
--- src/views/monitoring/node/nodeMonitoringBody.js
+++ src/views/monitoring/node/nodeMonitoringBody.js
@@ -4,7 +4,8 @@ const NodeMonitoringBody = props => {
                 <iframe title="grafana" 
                         width={"100%"} 
                         height={"100%"} 
-                        src={"http://192.168.0.6:30400/d/g4KR1lK4k/node-dashboard?orgId=1&refresh=5"} 
+                        // src={"http://192.168.0.6:30400/d/g4KR1lK4k/node-dashboard?orgId=1&refresh=5"} 
+                        src={"http://203.244.132.115:30400/d/g4KR1lK4k/node-dashboard?orgId=1&refresh=5"} 
                 />
         )
 }
